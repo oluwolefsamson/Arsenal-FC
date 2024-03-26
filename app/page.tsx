538e772bf75835ddfb4b5d65b444aedd8cc3095e
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-03-26 11:53:45 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
+import { ModeToggle } from "@/components/ToggleTheme";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+    < Navbar />
+   </>
+  );
+}
